bcf17a109222628dffc5149d873f91abc389c17c
Miracle <rjcvjchzljv9@gmail.com>
2020-02-21 12:01:32 +0200

    Homework done!

---
+++ HomeworkFibThread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkFibThread
+{
+    class Program
+    {
+        private delegate UInt64 AsyncFibDelegate(UInt64 n);
+        static void Main(string[] args)
+        {
+            AsyncFibDelegate del = Fib;
+            IAsyncResult ar1 = del.BeginInvoke(40, EndSum, del);
+            IAsyncResult ar2 = del.BeginInvoke(35, EndSum, del);
+            IAsyncResult ar3 = del.BeginInvoke(30, EndSum, del);
+
+
+            Console.ReadKey();
+        }
+        //метод вычисления ряда Фибоначчи
+        private static UInt64 Fib(UInt64 counter)
+        {
+            return counter == 0 || counter == 1 ? counter : Fib(counter - 1) + Fib(counter - 2);
+        }
+        //
+        private static void EndSum(IAsyncResult ar)
+        {
+            AsyncFibDelegate del = (AsyncFibDelegate)ar.AsyncState;
+            UInt64 result = del.EndInvoke(ar);
+            Console.WriteLine("Fib : " + result);
+        }
+    }
+}
